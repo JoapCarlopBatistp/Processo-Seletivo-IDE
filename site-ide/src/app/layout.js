62af772a39cb5875debd937017a886aaceba074d
JoapCarlop <joocarlos.batista@gmail.com>
2024-10-12 17:38:17 -0300

    Navbar e Footer colocados em componentes

---
+++ site-ide/src/app/layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './globals.css'
+import Footer from '@/components/Footer/Footer.jsx'
+import Navbar from '@/components/Navbar/Navbar.jsx'
+
+
+export default function App(){
+    return(
+        <html lang="en">
+            <body>
+                <h1>ola</h1>
+                <Navbar/>
+                <Footer/>
+            </body>
+        </html>
+    )
+}
